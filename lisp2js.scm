a3a7e2c0e216932e96e7d5069a47f9ba428285c7
tyfkda <tyfkda@gmail.com>
2014-11-03 22:39:23 +0900

    Compile pair into function call

--- lisp2js.scm
+++ lisp2js.scm
@@ -1,9 +1,28 @@
+(define (compile s)
+  (if (pair? s)
+      (compile-funcall s)
+    (compile-literal s)))
+
 (define (compile-literal s)
   (cond ((number? s) (number->string s))
-        (else (error "compile-literal"))))
+        ((symbol? s) (symbol->string s))
+        (else (error #`"compile-literal: [,s]"))))
+
+(define (compile-funcall s)
+  (define (expand-args args)
+    (string-join (map (lambda (x) (compile x))
+                      args)
+                 ", "))
+  (let ((fn (car s))
+        (args (cdr s)))
+    (string-append (compile fn)
+                   "("
+                   (expand-args args)
+                   ")")))
+
 
 (define (main args)
   (let ((ss (port->sexp-list (current-input-port))))
     (dolist (s ss)
-      (display (compile-literal s))
+      (display (compile s))
       (display ";\n"))))
