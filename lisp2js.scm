cf89ded57656f7307a1c60bafb0c71838fa580da
tyfkda <tyfkda@gmail.com>
2014-11-03 23:28:26 +0900

    Escape symbol characters except alphabet or numbers

--- lisp2js.scm
+++ lisp2js.scm
@@ -12,7 +12,25 @@
 
 (define (compile-symbol s)
   (string-append "LISP."
-                 (symbol->string s)))
+                 (symbol->js-string s)))
+
+(define (symbol->js-string sym)
+  (define (char->js-str c)
+    (if (sym-char? c)
+        (string c)
+      (string-append "_"
+                     (integer->hex-string (char->integer c) 2))))
+  (define (sym-char? c)
+    (alnum? c))
+  (define (integer->hex-string x keta)
+    (let* ((s (number->string x 16))
+           (l (string-length s)))
+      (if (< l keta)
+          (let1 zeros (make-string (- keta l) #\0)
+            (string-append zeros s))
+        s)))
+  (apply string-append (map char->js-str
+                            (string->list (symbol->string sym)))))
 
 (define (compile-funcall s)
   (define (expand-args args)
@@ -39,6 +57,16 @@
 (define (special-form? s)
   (cond ((assoc (car s) *special-forms*) => cdr)
         (else #f)))
+
+(define (alnum? c)
+  (or (alpha? c) (num? c)))
+
+(define (alpha? c)
+  (or (and (char<=? #\a c) (char<=? c #\z))
+      (and (char<=? #\A c) (char<=? c #\Z))))
+
+(define (num? c)
+  (and (char<=? #\0 c) (char<=? c #\9)))
 
 (define (main args)
   (let ((ss (port->sexp-list (current-input-port))))
