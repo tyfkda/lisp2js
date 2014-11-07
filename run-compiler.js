8b202530b8e871cc7daaba06faff17bb491e0ed5
tyfkda <tyfkda@gmail.com>
2014-11-07 12:05:00 +0900

    Accept JsLisp to stdin

--- run-compiler.js
+++ run-compiler.js
@@ -1,26 +1,48 @@
 (function() {
   require('./compiler');
 
-  if (process.argv.length < 3) {
-    console.error('arguments required');
-    process.exit(1);
-  }
+  // Read all input from stdin, and fire callback.
+  var readAllFromStdin = function(callback) {
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
 
-  var fs = require('fs');
-  var fileName = process.argv[2];
-  fs.readFile(fileName, 'utf-8', function(error, text) {
-    if (error) {
-      console.error('File open error [' + fileName + ']: ' + error);
-      process.exit(1);
-    }
+    var all = '';
+    process.stdin.on('data', function (chunk) {
+      all += chunk;
+    });
+    process.stdin.on('end', function () {
+      callback(all);
+    });
+  };
 
-    var reader = new LISP.SReader(text);
+  // Run codes.
+  var runCodes = function(codes) {
+    var reader = new LISP.SReader(codes);
+    var s;
     for (;;) {
-      var s = reader.read();
+      s = reader.read();
       if (!s)
         break;
 
       LISP.eval(s);
     }
-  });
+    return s === undefined;
+  };
+
+  if (process.argv.length < 3) {
+    readAllFromStdin(function(text) {
+      process.exit(runCodes(text) ? 0 : 1);
+    });
+  } else {
+    var fs = require('fs');
+    var fileName = process.argv[2];
+    fs.readFile(fileName, 'utf-8', function(error, text) {
+      if (error) {
+        console.error('File open error [' + fileName + ']: ' + error);
+        process.exit(1);
+      }
+
+      process.exit(runCodes(text) ? 0 : 1);
+    });
+  }
 })();
