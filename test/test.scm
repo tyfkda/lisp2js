3375bb1383cd49ad90f1a0a0d80dee4413a4accb
tyfkda <tyfkda@gmail.com>
2015-02-18 07:05:15 +0900

    Check type for number

--- test/test.scm
+++ test/test.scm
@@ -15,6 +15,8 @@
 (test "begin" 3 (begin 1 2 3))
 
 ;; type
+(test "type of int" 'number (type 123))
+(test "type of float" 'number (type 1.23))
 (test "type of pair" 'pair (type '(1 2 3)))
 (test "type of string" 'string (type "123"))
 (test "type of vector" 'vector (type #(1 2 3)))
