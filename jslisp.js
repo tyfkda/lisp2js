1960a0fd7351daeebf243535737afc2923f33b4f
tyfkda <tyfkda@gmail.com>
2015-02-17 20:14:24 +0900

    Add simple html sample

---
+++ jslisp.js
@@ -0,0 +1,24 @@
+/*
+  Usage:
+    <script src="jslisp.js">
+      (print (+ 1 2))
+    </script>
+ */
+
+(function() {
+  // Run Lisp codes.
+  var runCodes = function(codes) {
+    var reader = new LISP.Reader(codes);
+    for (;;) {
+      var s = reader.read();
+      if (s === undefined)
+        break;
+      LISP.eval(s);
+    }
+  };
+
+  var scriptTags = document.getElementsByTagName('script');
+  var myScriptTag = scriptTags[scriptTags.length - 1];
+  var codes = myScriptTag.text;
+  runCodes(codes);
+})();
