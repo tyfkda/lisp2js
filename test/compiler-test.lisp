2bccf80d7b3bbaab39db0dc627a636d773f7475e
tyfkda <tyfkda@gmail.com>
2015-02-28 13:08:10 +0900

    Reverse order for `local-var?`

--- test/compiler-test.lisp
+++ test/compiler-test.lisp
@@ -1,14 +1,14 @@
 (load "./test.lisp")
 
+(let1 scope (create-scope nil '(x y &rest z))
+  (test "local-var?[x]" t (local-var? scope 'x))
+  (test "local-var?[no]" nil (local-var? scope 'no))
+  (test "local-var?[rest]" t (local-var? scope 'z))
+  (test "local-var?[&rest]" nil (local-var? scope '&rest))
+  )
+
 (test "get-receiver with dot" 'foo (get-receiver 'foo.bar.baz))
 (test "get-receiver without dot" 'foo (get-receiver 'foo))
-
-(let1 scope (create-scope nil '(x y &rest z))
-  (test "local-var?[x]" t (local-var? 'x scope))
-  (test "local-var?[no]" nil (local-var? 'no scope))
-  (test "local-var?[rest]" t (local-var? 'z scope))
-  (test "local-var?[&rest]" nil (local-var? '&rest scope))
-  )
 
 (test "escape-char[A]" "A" (escape-char "A"))
 (test "escape-char[\\]" "\\\\" (escape-char "\\"))
