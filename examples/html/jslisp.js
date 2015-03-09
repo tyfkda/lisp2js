86067574ea072fb8656e4c509331edc4448fa259
tyfkda <tyfkda@gmail.com>
2015-03-10 08:04:51 +0900

    Update JS for html to use StrStream

--- examples/html/jslisp.js
+++ examples/html/jslisp.js
@@ -8,9 +8,9 @@
 (function() {
   // Run Lisp codes.
   var runCodes = function(codes) {
-    var reader = new LISP.Reader(codes);
+    var stream = new LISP.StrStream(codes);
     for (;;) {
-      var s = reader.read();
+      var s = LISP.read(stream);
       if (s === undefined)
         break;
       LISP.eval(s);
