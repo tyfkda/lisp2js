caf02d2818559fd53818d53f873a5b51ed6a8d69
tyfkda <tyfkda@gmail.com>
2015-12-28 05:14:43 +0900

    Remove semicolon from JS code

--- jslisp.js
+++ jslisp.js
@@ -6,19 +6,19 @@
  */
 
 (function() {
+  'use strict'
   // Run Lisp codes.
   var runCodes = function(codes) {
-    var stream = new LISP.StrStream(codes);
+    var stream = new LISP.StrStream(codes)
     for (;;) {
-      var s = LISP.read(stream);
+      var s = LISP.read(stream)
       if (s === undefined)
-        break;
-      LISP.eval(s);
+        break
+      LISP.eval(s)
     }
-  };
+  }
 
-  var scriptTags = document.getElementsByTagName('script');
-  var myScriptTag = scriptTags[scriptTags.length - 1];
-  var codes = myScriptTag.text;
-  runCodes(codes);
-})();
+  var scriptTags = document.getElementsByTagName('script')
+  var myScriptTag = scriptTags[scriptTags.length - 1]
+  runCodes(myScriptTag.text)
+})()
