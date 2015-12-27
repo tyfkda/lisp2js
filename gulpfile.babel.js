7125e650ceda82e3ba91d21de2e3dbf7db9ec721
tyfkda <tyfkda@gmail.com>
2015-12-27 15:27:25 +0900

    Prepare to use gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,32 @@
+import gulp from 'gulp'
+
+import babel from 'gulp-babel'
+import uglify from 'gulp-uglify'
+import sourcemaps from 'gulp-sourcemaps'
+import rename from 'gulp-rename'
+import gutil from 'gulp-util'
+
+const destDir = '.'
+
+gulp.task('default', [], () => {
+  console.log('dummy')
+})
+
+gulp.task('babel', () => {
+  gulp.src('./runtime.js')
+    .pipe(sourcemaps.init({loadMaps:true}))
+    .pipe(babel())
+    .pipe(rename('lisp2js.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(destDir))
+})
+
+gulp.task('uglify', () => {
+  gulp.src('./lisp2js.js')
+    .pipe(sourcemaps.init({loadMaps:true}))
+    .pipe(babel())
+    .pipe(uglify().on('error', gutil.log))
+    .pipe(rename('lisp2js.min.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(destDir))
+})
